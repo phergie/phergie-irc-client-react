2709437849fa9630bae8da07ed08a9c3ea527550
Matt Turland <mturland@synacor.com>
2013-06-29 15:50:55 -0400

    Fixed whitespace in WriteStream

--- src/Phergie/Irc/Client/React/WriteStream.php
+++ src/Phergie/Irc/Client/React/WriteStream.php
@@ -76,11 +76,11 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ircNick($nickname, $hopcount = null)
-	{
+    {
         $msg = $this->getGenerator()->ircNick($nickname, $hopcount);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $username
@@ -90,11 +90,11 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ircUser($username, $hostname, $servername, $realname)
-	{
+    {
         $msg = $this->getGenerator()->ircUser($username, $hostname, $servername, $realname);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $servername
@@ -103,11 +103,11 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ircServer($servername, $hopcount, $info)
-	{
+    {
         $msg = $this->getGenerator()->ircServer($servername, $hopcount, $info);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $user
@@ -115,22 +115,22 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ircOper($user, $password)
-	{
+    {
         $msg = $this->getGenerator()->ircOper($user, $password);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $message
      * @return string
      */
     public function ircQuit($message = null)
-	{
+    {
         $msg = $this->getGenerator()->ircQuit($message);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $server
@@ -138,11 +138,11 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ircSquit($server, $comment)
-	{
+    {
         $msg = $this->getGenerator()->ircSquit($server, $comment);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $channels
@@ -150,22 +150,22 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ircJoin($channels, $keys = null)
-	{
+    {
         $msg = $this->getGenerator()->ircJoin($channels, $keys);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $channels
      * @return string
      */
     public function ircPart($channels)
-	{
+    {
         $msg = $this->getGenerator()->ircPart($channels);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $target
@@ -174,11 +174,11 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ircMode($target, $mode, $param = null)
-	{
+    {
         $msg = $this->getGenerator()->ircMode($target, $mode, $param);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $channel
@@ -186,22 +186,22 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ircTopic($channel, $topic = null)
-	{
+    {
         $msg = $this->getGenerator()->ircTopic($channel, $topic);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $channels
      * @return string
      */
     public function ircNames($channels)
-	{
+    {
         $msg = $this->getGenerator()->ircNames($channels);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $channels
@@ -209,11 +209,11 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ircList($channels = null, $server = null)
-	{
+    {
         $msg = $this->getGenerator()->ircList($channels, $server);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $nickname
@@ -221,11 +221,11 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ircInvite($nickname, $channel)
-	{
+    {
         $msg = $this->getGenerator()->ircInvite($nickname, $channel);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $channel
@@ -234,22 +234,22 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ircKick($channel, $user, $comment = null)
-	{
+    {
         $msg = $this->getGenerator()->ircKick($channel, $user, $comment);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $server
      * @return string
      */
     public function ircVersion($server = null)
-	{
+    {
         $msg = $this->getGenerator()->ircVersion($server);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $query
@@ -257,11 +257,11 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ircStats($query, $server = null)
-	{
+    {
         $msg = $this->getGenerator()->ircStats($query, $server);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $servermask
@@ -269,22 +269,22 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ircLinks($servermask = null, $remoteserver = null)
-	{
+    {
         $msg = $this->getGenerator()->ircLinks($servermask, $remoteserver);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $server
      * @return string
      */
     public function ircTime($server = null)
-	{
+    {
         $msg = $this->getGenerator()->ircTime($server);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $targetserver
@@ -293,44 +293,44 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ircConnect($targetserver, $port = null, $remoteserver = null)
-	{
+    {
         $msg = $this->getGenerator()->ircConnect($targetserver, $port, $remoteserver);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $server
      * @return string
      */
     public function ircTrace($server = null)
-	{
+    {
         $msg = $this->getGenerator()->ircTrace($server);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $server
      * @return string
      */
     public function ircAdmin($server = null)
-	{
+    {
         $msg = $this->getGenerator()->ircAdmin($server);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $server
      * @return string
      */
     public function ircInfo($server = null)
-	{
+    {
         $msg = $this->getGenerator()->ircInfo($server);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $receivers
@@ -338,11 +338,11 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ircPrivmsg($receivers, $text)
-	{
+    {
         $msg = $this->getGenerator()->ircPrivmsg($receivers, $text);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $nickname
@@ -350,11 +350,11 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ircNotice($nickname, $text)
-	{
+    {
         $msg = $this->getGenerator()->ircNotice($nickname, $text);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $name
@@ -362,11 +362,11 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ircWho($name, $o = null)
-	{
+    {
         $msg = $this->getGenerator()->ircWho($name, $o);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $server
@@ -374,11 +374,11 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ircWhois($server, $nickmasks)
-	{
+    {
         $msg = $this->getGenerator()->ircWhois($server, $nickmasks);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $nickname
@@ -387,11 +387,11 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ircWhowas($nickname, $count = null, $server = null)
-	{
+    {
         $msg = $this->getGenerator()->ircWhowas($nickname, $count, $server);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $nickname
@@ -399,11 +399,11 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ircKill($nickname, $comment)
-	{
+    {
         $msg = $this->getGenerator()->ircKill($nickname, $comment);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $server1
@@ -411,11 +411,11 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ircPing($server1, $server2 = null)
-	{
+    {
         $msg = $this->getGenerator()->ircPing($server1, $server2);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $daemon
@@ -423,53 +423,53 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ircPong($daemon, $daemon2 = null)
-	{
+    {
         $msg = $this->getGenerator()->ircPong($daemon, $daemon2);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $message
      * @return string
      */
     public function ircError($message)
-	{
+    {
         $msg = $this->getGenerator()->ircError($message);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $message
      * @return string
      */
     public function ircAway($message = null)
-	{
+    {
         $msg = $this->getGenerator()->ircAway($message);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @return string
      */
     public function ircRehash()
-	{
+    {
         $msg = $this->getGenerator()->ircRehash();
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @return string
      */
     public function ircRestart()
-	{
+    {
         $msg = $this->getGenerator()->ircRestart();
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $user
@@ -477,33 +477,33 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ircSummon($user, $server = null)
-	{
+    {
         $msg = $this->getGenerator()->ircSummon($user, $server);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $server
      * @return string
      */
     public function ircUsers($server = null)
-	{
+    {
         $msg = $this->getGenerator()->ircUsers($server);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $text
      * @return string
      */
     public function ircWallops($text)
-	{
+    {
         $msg = $this->getGenerator()->ircWallops($text);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $nickname1
@@ -514,33 +514,33 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ircUserhost($nickname1, $nickname2 = null, $nickname3 = null, $nickname4 = null, $nickname5 = null)
-	{
+    {
         $msg = $this->getGenerator()->ircUserhost($nickname1, $nickname2, $nickname3, $nickname4, $nickname5);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $nicknames
      * @return string
      */
     public function ircIson($nicknames)
-	{
+    {
         $msg = $this->getGenerator()->ircIson($nicknames);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $receivers
      * @return string
      */
     public function ctcpFinger($receivers)
-	{
+    {
         $msg = $this->getGenerator()->ctcpFinger($receivers);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $nickname
@@ -548,22 +548,22 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ctcpFingerResponse($nickname, $text)
-	{
+    {
         $msg = $this->getGenerator()->ctcpFingerResponse($nickname, $text);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $receivers
      * @return string
      */
     public function ctcpVersion($receivers)
-	{
+    {
         $msg = $this->getGenerator()->ctcpVersion($receivers);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $nickname
@@ -573,22 +573,22 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ctcpVersionResponse($nickname, $name, $version, $environment)
-	{
+    {
         $msg = $this->getGenerator()->ctcpVersionResponse($nickname, $name, $version, $environment);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $receivers
      * @return string
      */
     public function ctcpSource($receivers)
-	{
+    {
         $msg = $this->getGenerator()->ctcpSource($receivers);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $nickname
@@ -598,22 +598,22 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ctcpSourceResponse($nickname, $host, $directories, $files)
-	{
+    {
         $msg = $this->getGenerator()->ctcpSourceResponse($nickname, $host, $directories, $files);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $receivers
      * @return string
      */
     public function ctcpUserinfo($receivers)
-	{
+    {
         $msg = $this->getGenerator()->ctcpUserinfo($receivers);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $nickname
@@ -621,22 +621,22 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ctcpUserinfoResponse($nickname, $text)
-	{
+    {
         $msg = $this->getGenerator()->ctcpUserinfoResponse($nickname, $text);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $receivers
      * @return string
      */
     public function ctcpClientinfo($receivers)
-	{
+    {
         $msg = $this->getGenerator()->ctcpClientInfo($receivers);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $nickname
@@ -644,11 +644,11 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ctcpClientinfoResponse($nickname, $client)
-	{
+    {
         $msg = $this->getGenerator()->ctcpClientinfoResponse($nickname, $client);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $receivers
@@ -656,11 +656,11 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ctcpErrmsg($receivers, $query)
-	{
+    {
         $msg = $this->getGenerator()->ctcpErrmsg($receivers, $query);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $nickname
@@ -669,11 +669,11 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ctcpErrmsgResponse($nickname, $query, $message)
-	{
+    {
         $msg = $this->getGenerator()->ctcpErrmsgResponse($nickname, $query, $message);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $receivers
@@ -681,11 +681,11 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ctcpPing($receivers, $timestamp)
-	{
+    {
         $msg = $this->getGenerator()->ctcpPing($receivers, $timestamp);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $nickname
@@ -693,22 +693,22 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ctcpPingResponse($nickname, $timestamp)
-	{
+    {
         $msg = $this->getGenerator()->ctcpPingResponse($nickname, $timestamp);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $receivers
      * @return string
      */
     public function ctcpTime($receivers)
-	{
+    {
         $msg = $this->getGenerator()->ctcpTime($receivers);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 
     /**
      * @param string $nickname
@@ -716,9 +716,9 @@ class WriteStream extends ReadableStream implements GeneratorInterface
      * @return string
      */
     public function ctcpTimeResponse($nickname, $time)
-	{
+    {
         $msg = $this->getGenerator()->ctcpTimeResponse($nickname, $time);
         $this->emit('data', array($msg));
         return $msg;
-	}
+    }
 }
