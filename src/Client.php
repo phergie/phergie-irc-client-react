e92f5f75cf5c2d04a2a75077583400c2ba859240
Chris Boden <cboden@gmail.com>
2014-08-20 19:48:37 -0400

    5.4 requirement, latest React Stream interface
    refs #15, refs #20

--- src/Client.php
+++ src/Client.php
@@ -22,7 +22,7 @@ use React\EventLoop\LoopInterface;
 use React\EventLoop\Timer\TimerInterface;
 use React\Promise\Deferred;
 use React\Stream\Stream;
-use React\Stream\StreamInterface;
+use React\Stream\DuplexStreamInterface;
 
 /**
  * IRC client implementation based on the React component library.
@@ -267,7 +267,7 @@ class Client extends EventEmitter implements
      *
      * @param resource $socket Socket for the connection to the server
      * @param \React\EventLoop\LoopInterface $loop Event loop
-     * @return \React\Stream\StreamInterface
+     * @return \React\Stream\DuplexStreamInterface
      */
     protected function getStream($socket)
     {
@@ -416,7 +416,7 @@ class Client extends EventEmitter implements
      * Returns a callback for when a connection is terminated, whether
      * explicitly by the bot or server or as a result of loss of connectivity.
      *
-     * @param \React\Stream\StreamInterface $stream Stream representing the
+     * @param \React\Stream\DuplexStreamInterface $stream Stream representing the
      *        connection to the server
      * @param \Phergie\Irc\ConnectionInterface $connection Terminated connection
      * @param \React\EventLoop\Timer\TimerInterface $timer Timer used to handle
@@ -424,7 +424,7 @@ class Client extends EventEmitter implements
      *        cancelled
      * @return callable
      */
-    protected function getEndCallback(StreamInterface $stream, ConnectionInterface $connection, TimerInterface $timer)
+    protected function getEndCallback(DuplexStreamInterface $stream, ConnectionInterface $connection, TimerInterface $timer)
     {
         $client = $this;
         $logger = $this->getLogger();
@@ -459,13 +459,13 @@ class Client extends EventEmitter implements
      *
      * @param \Phergie\Irc\ConnectionInterface $connection Metadata for the
      *        connection over which messages are being exchanged
-     * @param \React\Stream\StreamInterface $stream Stream representing the
+     * @param \React\Stream\DuplexStreamInterface $stream Stream representing the
      *        connection
      *        to the server
      * @param \Phergie\Irc\Client\React\WriteStream $write Stream used to
      *        send events to the server
      */
-    protected function configureStreams(ConnectionInterface $connection, StreamInterface $stream, WriteStream $write)
+    protected function configureStreams(ConnectionInterface $connection, DuplexStreamInterface $stream, WriteStream $write)
     {
         $timer = $this->addPeriodicTimer($this->getTickInterval(), $this->getTickCallback($write, $connection));
         $read = $this->getReadStream($connection);
@@ -582,7 +582,7 @@ class Client extends EventEmitter implements
         $this->getSecureConnector()
             ->create($hostname, $port)
             ->then(
-                function(StreamInterface $stream) use ($self, $connection) {
+                function(DuplexStreamInterface $stream) use ($self, $connection) {
                     $self->initializeStream($stream, $connection);
                     $self->emit('connect.after.each', array($connection, $connection->getOption('write')));
                 }
@@ -596,10 +596,10 @@ class Client extends EventEmitter implements
      * addSecureConnection() under PHP 5.3.x. It should not be called outside
      * this class.
      *
-     * @param \React\Stream\StreamInterface $stream
+     * @param \React\Stream\DuplexStreamInterface $stream
      * @param \Phergie\Irc\ConnectionInterface $connection
      */
-    public function initializeStream(StreamInterface $stream, ConnectionInterface $connection)
+    public function initializeStream(DuplexStreamInterface $stream, ConnectionInterface $connection)
     {
         try {
             $connection->setOption('stream', $stream);
