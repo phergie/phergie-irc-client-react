a8625e2a4170b9ea4e63f17aacc40beac1552fde
elazar <tobias382@gmail.com>
2014-12-24 11:27:09 -0600

    Forgot one change in the last commit

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -195,6 +195,8 @@ class ClientTest extends \PHPUnit_Framework_TestCase
         $command = $php . ' ' . $script . ' 2>&1';
         $output = shell_exec($command);
         $this->assertRegExp('/^[0-9]{4}(-[0-9]{2}){2} [0-9]{2}(:[0-9]{2}){2} DEBUG test \\[\\]$/', $output);
+
+        unlink($script);
     }
 
     /**
