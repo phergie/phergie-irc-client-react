b923fb0e4e026cf4eccf44c6385a4366a1386dbc
Renegade334 <contact.caaeed4f@renegade334.me.uk>
2015-05-26 19:37:51 +0400

    Use new phergie-irc-connection version, update composer dependencies

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -853,6 +853,7 @@ class ClientTest extends \PHPUnit_Framework_TestCase
         Phake::when($connection)->getNickname()->thenReturn('nickname');
         Phake::when($connection)->getServerHostname()->thenReturn('0.0.0.0');
         Phake::when($connection)->getServerPort()->thenReturn($this->port);
+        Phake::when($connection)->getMask()->thenReturn('nickname!username@servername');
         return $connection;
     }
 }
